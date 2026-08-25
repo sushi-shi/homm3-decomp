64c3d7ab3465779fed717a60ca19c9fd8c5ec8b8
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-25 20:54:22 +0000

    Match AI marketplace trading functions

--- include/ai_player.h
+++ include/ai_player.h
@@ -206,6 +206,8 @@ public:
     bool can_trade_resources(const int* cost, int* supply,
                              std::vector<long>& trade_qty);
     void trade_resources(const int* cost, long number);
+    bool build_markets(int* supply);
+    void do_resource_trade(int* supply);
 
 private:
     static float attack_computer_bonus;
