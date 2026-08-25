eb2df59c01a42de899883e73f08ed23d83659394
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-25 19:56:21 +0000

    Match single-selection dialog helpers

--- include/singleselectionwindow_priv.h
+++ include/singleselectionwindow_priv.h
@@ -1,12 +1,49 @@
 #ifndef HOMM3_SINGLESELECTIONWINDOW_PRIV_H
 #define HOMM3_SINGLESELECTIONWINDOW_PRIV_H
 
+#include "remotedlg.h"
 #include "slider.h"
+#include "textntry.h"
+#include "netmsg.h"
+#include "winmgr.h"
 
 class CChatSlider : public slider {
 public:
     virtual void SetResolution(int num);
     virtual void SetState(int state);
+};
+
+CNetMsg* GetRemoteData(unsigned char removeFromQueue,
+                       unsigned char* wasCompressed);
+
+class CHostWaitDlg : public CAnimatedDlg {
+public:
+    virtual ~CHostWaitDlg();
+    virtual int handle_message(message& msg);
+
+    CNetMsg* m_pMsg;
+    unsigned long m_forWho;
+};
+
+class CSingleSelectionNetMsgHandler : public CAdvMgrNetMsgHandler {
+public:
+    virtual CNetMsg* CheckHandleNet(unsigned char inPopup,
+                                    unsigned char* msgReceived);
+    virtual CNetMsg* HandleNetMsg(CNetMsg* pNetMsg);
+
+    unsigned char m_wasCompressed;
+};
+
+class CChatWidget : public textWidget {
+public:
+    class CChatSave : public Bitmap16Bit {
+    public:
+        unsigned char bSaved;
+        unsigned char IsSaved() const { return bSaved; }
+    };
+
+    virtual void Draw();
+    CChatSave* m_save;
 };
 
 #endif  /* HOMM3_SINGLESELECTIONWINDOW_PRIV_H */
