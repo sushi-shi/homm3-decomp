14b8073b027a526662d4b938fc4e40524665756a
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-31 12:19:25 +0000

    Make army wall family canonical

--- src/army.cpp
+++ src/army.cpp
@@ -24,7 +24,6 @@
 #define HOMM3_ARMY_PROTECTION_VIEW
 #define HOMM3_ARMY_SPELLS_VIEW
 #define HOMM3_ARMY_TURN_ABILITY_VIEW
-#define HOMM3_ARMY_WALL_VIEW
 #include <algorithm>
 #include <math.h>
 #include <stdlib.h>
