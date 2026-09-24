9337d76682d2d08adb62d5f30bef9d0c139777fd
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-09-25 01:12:47 +0200

    Use object type accessor in map placement

--- src/mapcell.cpp
+++ src/mapcell.cpp
@@ -4425,8 +4425,8 @@ int NewfullMap::placeObject(int objectIndex, unsigned char setExtraInfo)
     signed char heightMap[8][6];
     generateHeightMap(object, heightMap);
 
-    TAdventureObjectType objectClass =
-        g_game->m_worldMap.m_objectTypes[object->m_typeIndex].m_objectType;
+    // Mac retains getObjectTypePtr here at 0:0x128560; VC6 expands it.
+    TAdventureObjectType objectClass = object->getObjectTypePtr()->m_objectType;
 
     for (int col = 0; col < objectType->m_width; ++col) {
         if (object->m_x - col < 0 || object->m_x - col >= g_mapWidth)
