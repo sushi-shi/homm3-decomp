53a5ec07ed80c947e363f8b719992a983baa4dee
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-09-24 02:35:42 +0200

    Recover retail isHumanTeam loop in shared header

--- include/game.h
+++ include/game.h
@@ -1487,14 +1487,10 @@ VA(0x0042b9e0, 0x45)  // guarded team scan + named IsHuman callee, dc 0x37f64
 bool isHumanTeam(int teamNum) const
 {
     if (teamNum >= 0) {
-        int player = 0;
-        while (1) {
+        for (int player = 0; player < 8; ++player) {
             if (m_mapHeader.m_teamInfo[player] == teamNum
                 && g_game->isHuman(player))
                 return true;
-            ++player;
-            if (player >= 8)
-                break;
         }
     }
     return false;
